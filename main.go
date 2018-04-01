0c6a12af5061ce7fd3a1ef576541aae12a4bde74
eden90267 <eden90267@gmail.com>
2018-04-02 02:25:28 +0800

    variable, func

--- main.go
+++ main.go
@@ -1,13 +1,60 @@
 package main
 
 import (
-	"fmt"
-
-	_ "github.com/eden90267/go-first/bar"
-	_ "github.com/eden90267/go-first/foo"
+  "fmt"
+  "strings"
 )
 
+func getUserListSQL(username, email string, sexy int) string {
+  sql := "select * from user"
+  where := []string{}
+
+  if username != "" {
+    where = append(where, fmt.Sprintf("username = '%s'", username))
+  }
+  if email != "" {
+    where = append(where, fmt.Sprintf("email = '%s'", email))
+  }
+  if sexy != 0 {
+    where = append(where, fmt.Sprintf("sexy = '%d'", sexy))
+  }
+
+  return sql + " where " + strings.Join(where, " or ")
+}
+
+type searchOpts struct {
+  username string
+  email string
+  sexy int
+}
+
+func getUserListOptsSQL(opts searchOpts) string {
+  sql := "select * from user"
+  where := []string{}
+
+  if opts.username != "" {
+    where = append(where, fmt.Sprintf("username = '%s'", opts.username))
+  }
+  if opts.email != "" {
+    where = append(where, fmt.Sprintf("email = '%s'", opts.email))
+  }
+  if opts.sexy != 0 {
+    where = append(where, fmt.Sprintf("sexy = '%d'", opts.sexy))
+  }
+
+  return sql + " where " + strings.Join(where, " or ")
+}
+
 func main() {
-	//fmt.Print(HelloWorld())
-	fmt.Println("Hello main")
+  fmt.Println(getUserListSQL("eden90267", "", 0))
+  fmt.Println(getUserListSQL("eden90267", "eden90267@gmail.com", 1))
+
+  fmt.Println(getUserListOptsSQL(searchOpts{
+    username: "eden90267",
+    email: "eden90267@gmail.com",
+  }))
+
+  fmt.Println(getUserListOptsSQL(searchOpts{
+    sexy: 2,
+  }))
 }
